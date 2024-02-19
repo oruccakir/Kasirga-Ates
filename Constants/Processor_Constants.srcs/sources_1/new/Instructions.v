b2d0d5c6023accbd190d3d30fafd8ece066bd07d
oruccakir <oruccakir2525@gmail.com>
2024-02-19 08:55:11 +0300

    Instructions Constants module uploaded

---
+++ Constants/Processor_Constants.srcs/sources_1/new/Instructions.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.02.2024 08:51:41
+// Design Name: 
+// Module Name: Instructions
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Instructions(
+
+    );
+endmodule
