c197c13dc54e6ad12fb62f4d4cfbc1956d5e5f9f
oruccakir <oruccakir2525@gmail.com>
2024-03-04 17:52:21 +0300

    Integer Mul and Div modules created for execute step

---
+++ Pipeline/3-Execute/IntegerDivisionUnit.v
@@ -0,0 +1,11 @@
+// Purpose: Integer division unit for the execute stage of the pipeline.
+// Functionality: This module performs integer division.
+// File: IntegerDivisionUnit.v    
+
+module IntegerDivisionUnit(
+    input wire [31:0] operand1_i, // Operand 1 input
+    input wire [31:0] operand2_i, // Operand 2 input 
+    output wire [63:0] result_o // Result output
+);
+
+endmodule
---
+++ Pipeline/3-Execute/IntegerMultiplicationUnit.v
@@ -0,0 +1,13 @@
+// Purpose: 32-bit integer multiplication unit for the execute stage of the pipeline.
+// Functionality: This module performs 32-bit integer multiplication.
+// File: IntegerMultiplicationUnit.v
+
+module IntegerMultiplicationUnit(
+    input [31:0] operand1_i, // Operand 1 input
+    input [31:0] operand2_i, // Operand 2 input 
+    output wire [63:0] result_o // Result output
+);
+    
+   
+    
+endmodule
