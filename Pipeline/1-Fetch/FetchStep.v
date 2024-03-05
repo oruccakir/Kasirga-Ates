cbbc2f557e034c1120b17e83984e595a9c1947f6
oruccakir <oruccakir2525@gmail.com>
2024-03-05 17:03:37 +0300

    Fetch step input and outputs updated

--- Pipeline/1-Fetch/FetchStep.v
+++ Pipeline/1-Fetch/FetchStep.v
@@ -6,6 +6,8 @@ module FetchStep (
     input wire clk_i, // Clock input
     input wire rst_i, // Reset input
     input wire enable_step_i, // Enable input
+    input wire [31:0] mem_adres_i, // Memory address input
+    output wire [31:0] instruction_o // Instruction output
 );
     
 
