59dbbf1df1a7bd2fb39a31d5ab4b8eeccd05740a
oruccakir <oruccakir2525@gmail.com>
2024-03-05 22:00:17 +0300

    Tb and memory updated

--- Pipeline/1-Fetch/processor_tb.v
+++ Pipeline/1-Fetch/processor_tb.v
@@ -1,21 +1,21 @@
 
 module processor_tb();
 
-localparam BELLEK_ADRES = 32'h8000_0000;
+localparam MEMORY_ADDRESS = 32'h8000_0000;
 localparam ADRES_BIT = 32;
 localparam VERI_BIT = 32;
 
 reg clk_r;
 reg rst_r;
 
-wire [ADRES_BIT-1:0] islemci_bellek_adres;
+wire [ADRES_BIT-1:0] islemci_MEMORY_ADDRESS;
 wire [VERI_BIT-1:0] islemci_bellek_oku_veri;
 wire [VERI_BIT-1:0] islemci_bellek_yaz_veri;
 wire islemci_bellek_yaz;
 
 HelperMemory memory (
     .clk(clk_r),
-    .adres(islemci_bellek_adres),
+    .adres(islemci_MEMORY_ADDRESS),
     .oku_veri(islemci_bellek_oku_veri),
     .yaz_veri(islemci_bellek_yaz_veri),
     .yaz_gecerli(islemci_bellek_yaz)
@@ -24,7 +24,7 @@ HelperMemory memory (
 Processpr processor (
     .clk(clk_r),
     .rst(rst_r),
-    .bellek_adres(islemci_bellek_adres),
+    .MEMORY_ADDRESS(islemci_MEMORY_ADDRESS),
     .bellek_oku_veri(islemci_bellek_oku_veri),
     .bellek_yaz_veri(islemci_bellek_yaz_veri),
     .bellek_yaz(islemci_bellek_yaz)
@@ -65,27 +65,7 @@ initial begin
     repeat (10) @(posedge clk_r); #2; // 10 cevrim reset
     rst_r = 1'b0;
 
-    buyruk_kontrol(3); // 3 buyruk yurut
-    if (yazmac_oku(1) !== 5) begin
-        $display("[ERR] x1 DEGER HATASI expected: 5 actual: %0d", yazmac_oku(1));
-    end
-    if (yazmac_oku(2) !== 10) begin
-        $display("[ERR] x2 DEGER HATASI expected: 10 actual: %0d", yazmac_oku(2));
-    end
-    if (yazmac_oku(3) !== 15) begin
-        $display("[ERR] x3 DEGER HATASI expected: 15 actual: %0d", yazmac_oku(3));
-    end
-    if (islemci.ps_r !== 'h8000_000c) begin
-        $display("[ERR] program sayaci 4. buyrugu gostermeli.");
-    end
-
-    buyruk_kontrol(4); // 4 buyruk yurut
-    if (bellek_oku('h8000_0400) !== 32'hdeadbee0) begin
-        $display("[ERR] adres 0x80000400 DEGER HATASI expected: 0xdeadbee0 actual: 0x%0x.", bellek_oku('h8000_0400));
-    end
-    if (bellek_oku('h8000_0404) !== 32'hdeadbeef) begin
-        $display("[ERR] adres 0x80000404 DEGER HATASI expected: 0xdeadbeef actual: 0x%0x.", bellek_oku('h8000_0404));
-    end
+    
 
 end
 
@@ -146,7 +126,7 @@ function integer adres_satir_idx (
     input [ADRES_BIT-1:0] adres
 );
 begin
-    adres_satir_idx = (adres - BELLEK_ADRES) >> $clog2(VERI_BIT / 8);
+    adres_satir_idx = (adres - MEMORY_ADDRESS) >> $clog2(VERI_BIT / 8);
 end
 endfunction
 
