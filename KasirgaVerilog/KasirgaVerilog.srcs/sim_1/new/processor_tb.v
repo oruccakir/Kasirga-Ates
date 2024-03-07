eb1f2106d40d4691c2bf3c74b77437af04ee52ef
oruccakir <oruccakir2525@gmail.com>
2024-03-07 22:08:05 +0300

    Verşlog codes added

---
+++ KasirgaVerilog/KasirgaVerilog.srcs/sim_1/new/processor_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07.03.2024 22:06:22
+// Design Name: 
+// Module Name: processor_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module processor_tb(
+
+    );
+endmodule
