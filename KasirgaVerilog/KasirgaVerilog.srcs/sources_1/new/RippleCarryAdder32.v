6ff68c474cc090bacbbc27a4fc593d026c70ff0b
oruccakir <oruccakir2525@gmail.com>
2024-03-13 11:23:48 +0300

    ripple carry adder file created

---
+++ KasirgaVerilog/KasirgaVerilog.srcs/sources_1/new/RippleCarryAdder32.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.03.2024 11:23:24
+// Design Name: 
+// Module Name: RippleCarryAdder32
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RippleCarryAdder32(
+
+    );
+endmodule
