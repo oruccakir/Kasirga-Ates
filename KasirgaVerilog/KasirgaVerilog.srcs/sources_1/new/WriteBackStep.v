ba26f292c864f16c373c7097716d82d1d3a0c88d
oruccakir <oruccakir2525@gmail.com>
2024-03-15 17:35:12 +0300

    writeback step is updated

--- KasirgaVerilog/KasirgaVerilog.srcs/sources_1/new/WriteBackStep.v
+++ KasirgaVerilog/KasirgaVerilog.srcs/sources_1/new/WriteBackStep.v
@@ -40,32 +40,31 @@ assign isWorking = enable_step_i && writeback_finished != 1'b1; // Assign isWork
 integer i = 1;
 
 always @(posedge clk_i) begin
-    if(isWorking)
-        begin
-            case(STATE)
-                FIRST_CYCLE :
-                    begin
-                        writeback_working_info = 1'b1;
-                        $display(" WRITEBACK STEP Writing back to register file %d",calculated_result_i," for instruction %d",i);
-                        writebacked_result <= calculated_result_i; 
-                        reg_write_integer <= 1'b1;
-                        STATE <= SECOND_CYCLE; // Go to the second cycle
-                    end
-                SECOND_CYCLE :
-                    begin
-                            $display("-->Writeback completed for instruction num %d",i);
-                            $display("Writebacked result %d",writebacked_result_o);
-                            writeback_finished <= 1'b1;
-                            reg_write_integer <= 1'b0;
-                            i=i+1;
-                            STATE <= FIRST_CYCLE; // Go to the first cycle
-                            writeback_working_info = 1'b0;
-                    end
-                  STATE: begin
-                    STATE = SECOND_CYCLE;
-                  end
-            endcase
-        end
+    if(isWorking) begin
+        case(STATE)
+            FIRST_CYCLE :
+                begin
+                    writeback_working_info = 1'b1;
+                    $display(" WRITEBACK STEP Writing back to register file %d",calculated_result_i," for instruction %d",i);
+                    writebacked_result <= calculated_result_i; 
+                    reg_write_integer <= 1'b1;
+                    STATE <= SECOND_CYCLE; // Go to the second cycle
+                end
+            SECOND_CYCLE :
+                begin
+                        $display("-->Writeback completed for instruction num %d",i);
+                        $display("Writebacked result %d",writebacked_result_o);
+                        writeback_finished <= 1'b1;
+                        reg_write_integer <= 1'b0;
+                        i=i+1;
+                        STATE <= FIRST_CYCLE; // Go to the first cycle
+                        writeback_working_info = 1'b0;
+                end
+              STATE: begin
+                STATE = SECOND_CYCLE;
+              end
+        endcase
+    end
 end
 
 assign writeback_finished_o = writeback_finished; // Assign writeback_finished
