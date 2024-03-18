9b3f6aec6c48c0356a79501b90c03df1fd314694
oruccakir <oruccakir2525@gmail.com>
2024-03-18 11:29:20 +0300

    Copy project uploaded

---
+++ KasırgaVerilogCopy/KasirgaVerilogCopy.srcs/sources_1/new/FloatingPointUnit.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 18.03.2024 11:25:55
+// Design Name: 
+// Module Name: FloatingPointUnit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module FloatingPointUnit(
+
+    );
+endmodule
