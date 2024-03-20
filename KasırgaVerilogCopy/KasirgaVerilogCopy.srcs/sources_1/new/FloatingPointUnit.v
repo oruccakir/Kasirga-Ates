b8d92c47e1292242c4a4f3440717b9ef5ef791aa
oruccakir <oruccakir2525@gmail.com>
2024-03-20 17:44:00 +0300

    fpu updated

--- KasırgaVerilogCopy/KasirgaVerilogCopy.srcs/sources_1/new/FloatingPointUnit.v
+++ KasırgaVerilogCopy/KasirgaVerilogCopy.srcs/sources_1/new/FloatingPointUnit.v
@@ -15,6 +15,9 @@ module FloatingPointUnit (
 
 reg enable_float_multiplication = 1'b0; // Enable float multiplication
 reg enable_float_division = 1'b0; // Enable float division
+
+wire [31:0] mul_result; // Multiplication result
+wire [31:0] div_result; // Division result
 // Floting multiplication unit
 FloatingMultiplicationUnit fmulUnit (
     .clk_i(clk_i), // Clock input
@@ -22,7 +25,7 @@ FloatingMultiplicationUnit fmulUnit (
     .enable_i(enable_float_multiplication), // Enable input
     .operand1_i(operand1_i), // Operand 1 input
     .operand2_i(operand2_i), // Operand 2 input
-    .result_o(result_o) // Result output
+    .result_o(mul_result) // Result output
 );
 
 // Floating division unit
@@ -32,8 +35,32 @@ FloatingDivisionUnit fdivUnit (
     .enable_i(enable_float_division), // Enable input
     .operand1_i(operand1_i), // Operand 1 input
     .operand2_i(operand2_i), // Operand 2 input
-    .result_o(result_o) // Result output
+    .result_o(div_result) // Result output
 );
+
+
+always@(*) begin
+   if(enable_i == 1'b1) begin
+       case(floatOp_i)
+           `FLT_FMUL: begin
+               enable_float_multiplication = 1'b1;
+           end
+           `FLT_FDIV: begin
+               enable_float_division = 1'b1;
+           end
+           default: begin
+               enable_float_multiplication = 1'b0;
+               enable_float_division = 1'b0;
+           end
+       endcase
+   end
+   else begin
+       enable_float_multiplication = 1'b0;
+       enable_float_division = 1'b0;
+   end
+end
+
+assign result_o = (floatOp_i == `FLT_FMUL) ? mul_result : div_result;
 
 endmodule
 
