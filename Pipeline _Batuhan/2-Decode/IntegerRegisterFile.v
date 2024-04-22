62adaacb6574c090e425c63ff2653367e112cbd3
BatuhanUzunoglu <105708655+BatuhanUzunoglu@users.noreply.github.com>
2024-04-22 16:08:50 +0300

    IntegerRegisterFile.v

--- Pipeline _Batuhan/2-Decode/IntegerRegisterFile.v
+++ Pipeline _Batuhan/2-Decode/IntegerRegisterFile.v
@@ -26,8 +26,38 @@ assign read_data2_o = register_file[rs2_i];
 
 always@(posedge clk_i) begin
   if(rst_i)begin
-   register_file[0] <= 0;
-   register_file[2] <= stack_address;
+   register_file[0]  <= 32'd0;
+   register_file[1]  <= 32'd1;
+   register_file[2]  <= 32'd2;
+   register_file[3]  <= 32'd3;
+   register_file[4]  <= 32'd4;
+   register_file[5]  <= 32'd5;
+   register_file[6]  <= 32'd6;
+   register_file[7]  <= 32'd7;
+   register_file[8]  <= 32'd8;
+   register_file[9]  <= 32'd9;
+   register_file[10] <= 32'd10;
+   register_file[11] <= 32'd11;
+   register_file[12] <= 32'd12;
+   register_file[13] <= 32'd13;
+   register_file[14] <= 32'd14;
+   register_file[15] <= 32'd15;
+   register_file[16] <= 32'd16;
+   register_file[17] <= 32'd17;
+   register_file[18] <= 32'd18;
+   register_file[19] <= 32'd19;
+   register_file[20] <= 32'd20;
+   register_file[21] <= 32'd21;
+   register_file[22] <= 32'd22;
+   register_file[23] <= 32'd23;
+   register_file[24] <= 32'd24;
+   register_file[25] <= 32'd25;
+   register_file[26] <= 32'd26;
+   register_file[27] <= 32'd27;
+   register_file[28] <= 32'd28;
+   register_file[29] <= 32'd29;
+   register_file[30] <= 32'd30;
+   register_file[31] <= 32'd31;
 end
   else if(reg_write_i && (rd_i !=0))begin
     register_file[rd_i] <= write_data_i;
