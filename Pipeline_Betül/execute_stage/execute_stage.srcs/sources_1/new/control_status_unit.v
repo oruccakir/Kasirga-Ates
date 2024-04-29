1fc84b4c0fb7c22e2661f106b689d62ef9157c61
Batuhan Uzunoglu <b.uzunoglu@etu.edu.tr>
2024-04-29 14:32:50 +0300

    gereksiz dosyalar silindi, dosya isimleri degistirildi

--- Pipeline_Betül/execute_stage/execute_stage.srcs/sources_1/new/control_status_unit.v
+++
@@ -1,8 +0,0 @@
-`timescale 1ns / 1ps
-module control_status_unit(
-   input                                                        clk_i,
-   input                                                        rst_i,
-   input                                                        enable_control_status_unit_i,
-   output                      reg                                 finished_o
-    );
-endmodule
